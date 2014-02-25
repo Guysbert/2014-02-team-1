a8bda8db415ddc300986ba63cd7c2887f8be2bd7
cc-training01 <benedikt.ritter@codecentric.de>
2014-02-25 16:05:52 +0100

    Add javadoc comment to make sonar happy

--- worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/IsbnAlreadyUsedException.java
+++ worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/IsbnAlreadyUsedException.java
@@ -1,5 +1,11 @@
 package de.codecentric.psd.worblehat.domain;
 
+/**
+ * An exception showing that an ISBN is already in use.
+ * 
+ * @author schulung
+ * 
+ */
 public class IsbnAlreadyUsedException extends Exception {
 
 	private static final long serialVersionUID = -211233096486850007L;
